6ca72b888f936037ed07d39c17372f748a427797
santoshk32 <santosht165@outlook.com>
2024-04-01 17:54:31 +0000

    Added signup form and animation

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,33 @@
-const email = document.getElementById("email")
+
+const loginContent = document.getElementById('loginForm')
+const signupContent = document.getElementById('signupForm')
+const rightHeader = document.getElementById('authHeading')
+const leftHeader = document.getElementById('activeSignup')
+let isSignupClicked = false
+signupContent.classList = 'right-flex-hide'
+
+function ToggleAuth() {
+    if (!isSignupClicked) {
+        rightHeader.innerHTML = "Login "
+        leftHeader.innerHTML = "Login"
+        signupContent.classList = 'right-flex'
+        loginContent.classList = 'right-flex-hide'
+        isSignupClicked = true
+    }
+    else {
+      
+        rightHeader.innerHTML = "Login"
+        leftHeader.innerHTML = "Signup"
+        signupContent.classList = 'right-flex-hide'
+        loginContent.classList = 'right-flex'
+        isSignupClicked = false
+    }
+}
+
+const signUp = document.getElementById("activeSignup")
+signUp.addEventListener('click', ToggleAuth)
+
+const email = document.getElementById("mail")
 const pswd = document.getElementById("pswd")
 
 const btn = document.getElementById('loginBtn')
@@ -7,18 +36,19 @@ let emailInp;
 let pswdInp;
 
 email.addEventListener('blur', (e) => {
-  emailInp = e.target.value
+    emailInp = e.target.value
 })
 
 pswd.addEventListener('blur', (e) => {
-  pswdInp = e.target.value
+    pswdInp = e.target.value
 })
 
 btn.addEventListener('click', () => {
-  if (emailInp && pswdInp) {
-    window.location.pathname = '/src/quiz/quiz.html'
-  }
-  else {
-    alert("Invalid Email or Password")
-  }
+    console.log(emailInp, pswdInp)
+    if (emailInp && pswdInp) {
+        window.location.pathname = '/src/quiz/quiz.html'
+    }
+    else {
+        alert("Invalid Email or Password")
+    }
 })
