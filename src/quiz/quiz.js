7f5492a3c9aaddcf921cbad95c50ab850df90168
santoshk32 <santosht165@outlook.com>
2024-04-02 10:12:44 +0000

    Functionality added for quiz

--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -1,3 +1,5 @@
+import data from './DummyData.json'
+
 
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
@@ -16,8 +18,38 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
+let questionNumber=0
 window.onload = function () {
     var fiveMinutes = 60 * 1,
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
+
+    loadQuestion(questionNumber)
 };
+const addNum = document.getElementById('qNum')
+
+const optionOne=document.getElementById('optOne')
+const optionTwo=document.getElementById('optTwo')
+const optionThree=document.getElementById('optThree')
+const optionFour=document.getElementById('optFour')
+
+function loadQuestion(num){
+    const putQuestion = document.getElementById('questionText')
+    const firstData=data[num]
+        putQuestion.innerHTML=firstData.question
+        addNum.innerHTML=questionNumber+1
+        optionOne.innerHTML=firstData.A
+        optionTwo.innerHTML=firstData.B
+        optionThree.innerHTML=firstData.C
+        optionFour.innerHTML=firstData.D
+}
+
+const getNextClick=document.getElementById('nextBtn')
+
+
+getNextClick.addEventListener('click',()=>{
+    questionNumber=questionNumber+1
+    loadQuestion(questionNumber)
+    addNum.innerHTML=questionNumber+1
+   
+})
